1a7a12a1bcea6b747f30d88bd96335ee263c48e5
Flaye <45353885+Flaye@users.noreply.github.com>
2022-03-19 15:25:16 +0100

    :memo: code

--- PythonFiles/nlp.py
+++ PythonFiles/nlp.py
@@ -3,13 +3,16 @@ from data_import import *
 import os
 
 def extractEntity(nlp, entityOrIntent):
+    print(entityOrIntent,"\n", nlp)
     if entityOrIntent == "intents":
         if (len(nlp['intents']) != 0):
             if (nlp['intents'][0]['confidence'] >= 0.8):
                 return nlp['intents'][0]
     else:
-        if (nlp['entities'][entityOrIntent][0]['confidence'] >= 0.8):
-            return nlp['entities'][entityOrIntent][0]['value']
+        if ((entityOrIntent in nlp['entities']) and nlp['entities'][entityOrIntent][0]['confidence'] >= 0.8):
+            for i in range(len(nlp['entities'][entityOrIntent])):
+                if nlp['entities'][entityOrIntent][i]['value'] != "Where" and nlp['entities'][entityOrIntent][i]['value'] != "in":
+                    return nlp['entities'][entityOrIntent][i]['value']
     return None
 
 
@@ -20,39 +23,83 @@ def NLP(msg):
 
     if intent:
         if intent['name'] == "find_by_type": #Looking for french food
-            typefood = extractEntity(nlp, 'TypeFood:TypeFood')
-            all_placeId = chefmozcuisine.index[chefmozcuisine['Rcuisine'].str.lower() == typefood.lower()]
-            if(len(all_placeId) != 0):
-                print(all_placeId)
-                list_restaurant = geoplaces2.loc[geoplaces2.index.isin(all_placeId)] #Get all rows where id is in list of id :all_placeId
-                if(len(list_restaurant)!=0):
-                    list_five = list_restaurant.sample(n = 3)
-                    dict_place = []
-                    for index, row in list_five.iterrows():
-                        val = {'txt':"Name : {}\nAddress : {}\nCity : {}\nPrice : ".format(row["name"],row["address"],row["city"],row["price"]), "link":"https://www.google.com/maps/place/{},{}".format(row["latitude"],row["longitude"])}
-                        dict_place.append(val)
-                    return dict_place
-            return [{'txt': "Sorry, no restaurant found !", "link":None}]
-
+            try:
+                typefood = extractEntity(nlp, 'TypeFood:TypeFood')
+                all_placeId = chefmozcuisine.index[chefmozcuisine['Rcuisine'].str.lower() == typefood.lower()]
+                if(len(all_placeId) != 0):
+                    print(all_placeId)
+                    list_restaurant = geoplaces2.loc[geoplaces2.index.isin(all_placeId)] #Get all rows where id is in list of id :all_placeId
+                    if(len(list_restaurant)!=0):
+                        list_five = list_restaurant.sample(n = 5)
+                        dict_place = []
+                        for index, row in list_five.iterrows():
+                            val = {'txt':"Name : {}\nAddress : {}\nCity : {}\nPrice : ".format(row["name"],row["address"],row["city"],row["price"]), "link":"https://www.google.com/maps/place/{},{}".format(row["latitude"],row["longitude"])}
+                            dict_place.append(val)
+                        return dict_place
+                return [{'txt': "Sorry, no restaurant found !", "link":None}]
+            except:
+                print("Erreur ligne 39")
+                return [{'txt': "Sorry, something went wrong !", "link": None}]
         elif intent['name'] == "get_info": #Info for the restaurant <name of restaurant>
-            restaurant = extractEntity(nlp, 'restaurant:restaurant')
+            try:
+                restaurant = extractEntity(nlp, 'restaurant:restaurant')
+                print(restaurant)
+            except:
+                print("Erreur ligne 45")
+                return [{'txt': "Sorry, something went wrong !", "link": None}]
+
             city = None
             try: #Maybe in a specific city
                 city = extractEntity(nlp, 'Localisation:City')
                 print(city)
             except:
-                print("No specific city in the request")
+                print("No specific city in the request, ligne 53")
+
+            copy_geoplaces2 = geoplaces2.copy()
+            copy_geoplaces2["name"] = copy_geoplaces2["name"].str.lower()
+            rest_info = copy_geoplaces2.loc[copy_geoplaces2["name"].str.contains(restaurant.lower())]
+            if(city != None):
+                rest_info = rest_info.loc[rest_info["city"].str.lower() == city.lower()]
+            if(len(rest_info) != 0):
+                dict_place = []
+                for index, row in rest_info.iterrows():
+                    val = {'txt': "Name : {}\nAddress : {}\nCity : {}\nPrice : ".format(row["name"], row["address"],
+                                                                                        row["city"], row["price"]),
+                           "link": "https://www.google.com/maps/place/{},{}".format(row["latitude"],
+                                                                                    row["longitude"])}
+                    dict_place.append(val)
+                return dict_place
+            return [{'txt': "Sorry, no restaurant found !", "link":None}]
+
         elif intent['name'] == "find_by_city": #Give the 5 best restaurant in the city
             city = extractEntity(nlp, "Localisation:City")
+            print(city)
+            list_restaurant = geoplaces2.loc[geoplaces2['city'].str.lower() == city.lower()]
+
             typefood = None
-            try: #Give the best for a specific type of food
+            try:  # Give the best for a specific type of food
                 typefood = extractEntity(nlp, 'TypeFood:TypeFood')
             except:
                 print("No specific type of food")
+            print(typefood)
+            try:
+                if(typefood != None):
+                    all_placeId = chefmozcuisine.index[chefmozcuisine['Rcuisine'].str.lower() == typefood.lower()]
+                    list_restaurant = list_restaurant.index[list_restaurant.index.isin(all_placeId)]
 
-        # return {txt:messageWithBDD, link:google.com/maps/place}
-        return [{'txt': "Wait for the data", 'link': "https://www.google.com/maps/place/22.14958,-100.999557"}]
+                if (len(list_restaurant) != 0):
+                    list_five = list_restaurant.sample(n=5)
+                    dict_place = []
+                    for index, row in list_five.iterrows():
+                        val = {'txt': "Name : {}\nAddress : {}\nCity : {}\nPrice : ".format(row["name"], row["address"],
+                                                                                            row["city"], row["price"]),
+                               "link": "https://www.google.com/maps/place/{},{}".format(row["latitude"],
+                                                                                        row["longitude"])}
+                        dict_place.append(val)
+                    return dict_place
+                return [{'txt': "Sorry, no restaurant found !", "link": None}]
+            except:
+                return [{'txt': "Sorry, something went wrong !", "link": None}]
+        return [{'txt': "Could you ask again ?", 'link': None}]
     else:
         return [{'txt': "Could you ask again ?", 'link': None}]
-    # rep = client.message(msg)
-    # return rep
